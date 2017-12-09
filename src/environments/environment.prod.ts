915f3e2bd612f1259f6439eb0327aec17e0322ca
Guadalupe de Jesus Morales Bocanegra <luponksoft@gmail.com>
2017-12-09 13:57:14 -0600

    Saving build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://jsonplaceholder.typicode.com/albums',
+  apiKey: '3c8434b12fecd2bb06737071df65d898',
+  lastFMUrl: 'http://ws.audioscrobbler.com/2.0/?',
+  lastFMArtistSearch: 'method=artist.search&artist=SEARCH_STRING&api_key=3c8434b12fecd2bb06737071df65d898&format=json',
+  lastFMArtistTopAlbums: 'method=artist.gettopalbums&artist=SEARCH_STRING&api_key=3c8434b12fecd2bb06737071df65d898&format=json',
+  lastFMAlbumDetails: 'method=album.getinfo&api_key=3c8434b12fecd2bb06737071df65d898&artist=ARTIST_NAME&album=ALBUM_NAME&format=json'
 };
